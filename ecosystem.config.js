7f9dfb6c66db4e6e1557d2c5131e98161f4235c9
Robert Tod <roberttod@gmail.com>
2016-12-24 16:19:27 +0000

    Add PM2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'tutorial-2',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-52-209-166-225.eu-west-1.compute.amazonaws.com',
+      key: '~/.ssh/tutorial-2.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:roberttod/tutorial-pt-2.git',
+      path: '/home/ubuntu/tutorial-2',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
