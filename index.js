f1ad8667c9dcacee479d718901b5237fdaf0338f
Robert Tod <roberttod@gmail.com>
2016-12-24 16:48:49 +0000

    Simple HTML file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
-})
+
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
