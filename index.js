b49dac0f9321a3978284ffafbe8770ed96c0896d
Robert Tod <robert@qubit.com>
2017-01-23 13:26:54 -0500

    log visitors

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 const express = require('express')
-const app = express()
+const { MongoClient } = require('mongodb')
 
-app.use(express.static('public'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+const mongoUrl = 'mongodb://localhost:27017/tutorial-app'
+MongoClient.connect(mongoUrl, (err, db) => {
+  if (err) {
+    console.log('Failed to connect to MongoDB')
+    throw err
+  }
+
+  const app = express()
+  const logs = db.collection('logs')
+
+  app.use(express.static('public'))
+
+  app.get('/log.js', (req, res) => {
+    logs.insertOne({
+      ts: Date.now(),
+      ua: req.headers['user-agent'],
+      referer: req.headers['referer'],
+      ip: getIp(req)
+    }, (err) => {
+      if (err) {
+        console.log('Failed to create log')
+        throw err
+      }
+      res.send(`console.log('You got tracked')`)
+    })
+  })
+  app.listen(3000, () => {
+    console.log('Server running on port 3000')
+  })
+})
+
+function getIp (req) {
+  return req.headers['x-real-ip'] ||
+    req.connection.remoteAddress
+}
